44756b24df0f8aaf0e0903c9c991104e24741b9b
Nethsara Siyum <nethsarasiyum2@gmail.com>
2022-06-01 22:08:57 +0530

    Update mail.php

--- php contact form with ajax/mail.php
+++ php contact form with ajax/mail.php
@@ -1,31 +1,17 @@
 <?php
 
-$companyName = $_POST['companyName'];
+$name = $_POST['name'];
 $address = $_POST['address'];
-$wayBillAddress = $_POST['wayBillAddress'];
-$managerName = $_POST['managerName'];
-$nic = $_POST['nic'];
 $tele = $_POST['tele'];
 $email = $_POST['email'];
-$description = $_POST['description'];
-$bankDetails = $_POST['bankDetails'];
-$rates = $_POST['rates'];
-$comments = $_POST['comments'];
-$courierService = $_POST['courierService'];
-$storeType = $_POST['storeType'];
-$tier = $_POST['tier'];
-$confirmation = $_POST['confirmation'];
-$cod = $_POST['cod'];
 
 
-
-
-$subject = "New Response on Ezip";
+$subject = "New Response on from Website";
 
 
 
 $recipient = "nethsarasiyum2@gmail.com";
-$body = "Name: $companyName\n Address: $address\n Way Bill Address: $wayBillAddress\n Manager Name: $managerName\n NIC: $nic\n TelePhone:$tele \n Email: $email\n Product Description: $description \n Bank Details : $bankDetails\n Rates : $rates \n Courier Service Selection: $courierService\n Store Type: $storeType \n Tier: $tier \n Confirmation : $confirmation \n COD Rules : $cod \n Comments : $comments \n\n\nEmail Sent From ezip";
+$body = "Name: name\n Address: $address\n  TelePhone:$tele \n Email: $email ";
 $sender = "From: me@siyum.com";
 
 
